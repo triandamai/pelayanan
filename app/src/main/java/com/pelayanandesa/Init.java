512b79f064ab5fa588fd296f1d17e35da39c81f4
Trian Damai <triannurizkillah@gmail.com>
2020-03-05 17:35:10 +0700

    add auth(login)

--- app/src/main/java/com/pelayanandesa/Init.java
+++ app/src/main/java/com/pelayanandesa/Init.java
@@ -9,6 +9,6 @@ public class Init extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_init);
     }
 }
--- auth/src/main/java/com/auth/Login.java
+++ auth/src/main/java/com/auth/Login.java
@@ -1,14 +1,40 @@
 package com.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Login extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @OnClick({R.id.toolbar, R.id.appbar})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.toolbar:
+                break;
+            case R.id.appbar:
+                break;
+        }
     }
 }
