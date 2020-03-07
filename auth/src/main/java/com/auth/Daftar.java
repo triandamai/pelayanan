624dc304195cabc26b42aeaea14d454bdf77bed3
Scovandis <Andriyan354354@gmail.com>
2020-03-08 00:03:03 +0700

    helloWorld

--- auth/src/main/java/com/auth/Daftar.java
+++ auth/src/main/java/com/auth/Daftar.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Daftar extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
