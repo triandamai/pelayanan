8dc4c7f22297d852029d7ba840b0890fd377d1f0
Trian Damai <triannurizkillah@gmail.com>
2020-07-22 13:52:41 +0700

    add conversation

---
+++ app/src/main/java/com/tdn/laporan_desa/ui/chat/ConversationFragment.java
@@ -0,0 +1,36 @@
+package com.tdn.laporan_desa.ui.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.tdn.laporan_desa.R;
+
+public class ConversationFragment extends Fragment {
+
+    private ConversationViewModel mViewModel;
+
+    public static ConversationFragment newInstance() {
+        return new ConversationFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.conversation_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ConversationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/tdn/laporan_desa/ui/chat/ConversationViewModel.java
@@ -0,0 +1,7 @@
+package com.tdn.laporan_desa.ui.chat;
+
+import androidx.lifecycle.ViewModel;
+
+public class ConversationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
